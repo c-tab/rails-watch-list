1838678072772f8665002f51ab7068079bdc45c0
Carlos Aguirre <carlostomasaguirre@yahoo.com>
2023-05-25 16:40:50 +0200

    progress

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,31 @@
+class BookmarksController < ApplicationController
+  before_action :set_list, only: ['new', 'create']
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    @bookmark.save
+    redirect_to list_path(@list)
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:bookmark_id])
+    @bookmark.delete
+    redirect_to lists_path, status: :see_other
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'lists#home'
+  resources :lists, only: [:index, :show, :create, :new] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
+  resources :movies, only: [:index]
 end
